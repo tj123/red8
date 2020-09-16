edc58ed0e239e9728712406300d776135418b155
tj <tang123jian@126.com>
2020-09-17 01:22:46 +0800

    说明

--- src/main/java/com/shundian/red/web/controller/RedisController.java
+++ src/main/java/com/shundian/red/web/controller/RedisController.java
@@ -1,5 +1,7 @@
 package com.shundian.red.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,13 @@ import com.shundian.red.common.Result;
 import com.shundian.red.web.pojo.UserDto;
 import com.shundian.red.web.service.RedisService;
 
+
+
 @Controller
 @RequestMapping("/redis")
 public class RedisController {
+
+    Logger logger = LoggerFactory.getLogger(RedisController.class);
 
 	@Autowired
 	private RedisService service;
@@ -24,6 +30,7 @@ public class RedisController {
 	@RequestMapping("/list")
 	@ResponseBody
 	public Result<?> list(){
+	    logger.debug("这是一个嵯峨山上石搜索");
 		Result<Object> res = new Result<>();
 		return res.setData(service.listUser());
 	}
