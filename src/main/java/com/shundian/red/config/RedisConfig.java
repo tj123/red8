1b4d839d6e00d0c7afc0cd0aeb002be1e17c4c9d
tj <tang123jian@126.com>
2020-09-19 20:25:03 +0800

    说明

--- src/main/java/com/shundian/red/config/RedisConfig.java
+++ src/main/java/com/shundian/red/config/RedisConfig.java
@@ -1,11 +1,17 @@
 package com.shundian.red.config;
 
+import com.shundian.red.TestBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 //@Configuration
 public class RedisConfig {
 
-
+//    @Bean
+    public TestBean testBean(){
+        System.out.println("初始化 testBean");
+        return new TestBean();
+    }
 
 
 
